cf440740a4f6047d87622f4531be1619944c1efe
Omar Khaled <96246719+OmarKhaled001@users.noreply.github.com>
2025-01-17 03:23:15 +0200

    Update AdminPanelProvider.php

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -56,9 +56,9 @@ class AdminPanelProvider extends PanelProvider
                 DisableBladeIconComponents::class,
                 DispatchServingFilamentEvent::class,
             ])
-            // ->plugin(\BezhanSalleh\FilamentShield\FilamentShieldPlugin::make())
-            // ->plugin(\TomatoPHP\FilamentUsers\FilamentUsersPlugin::make())
-            // ->plugin(ActivitylogPlugin::make())
+             ->plugin(\BezhanSalleh\FilamentShield\FilamentShieldPlugin::make())
+             ->plugin(\TomatoPHP\FilamentUsers\FilamentUsersPlugin::make())
+             ->plugin(ActivitylogPlugin::make())
 
             ->authMiddleware([
                 Authenticate::class,
